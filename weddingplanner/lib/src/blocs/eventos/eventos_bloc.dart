0ae617707a55f253cf328ddcc51d3ea08830415b
Jose A. Cadena <jose.cadena@grupotum.com>
2021-06-24 11:59:58 -0500

    card detalles evento

--- weddingplanner/lib/src/blocs/eventos/eventos_bloc.dart
+++ weddingplanner/lib/src/blocs/eventos/eventos_bloc.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:weddingplanner/src/logic/eventos_logic.dart';
+import 'package:weddingplanner/src/models/item_model_evento.dart';
 import 'package:weddingplanner/src/models/item_model_eventos.dart';
 
 part 'eventos_event.dart';
@@ -16,43 +17,36 @@ class EventosBloc extends Bloc<EventosEvent, EventosState> {
   Stream<EventosState> mapEventToState(
     EventosEvent event,
   ) async* {
-    if(event is FechtEventosEvent){
+    if (event is FechtEventosEvent) {
       yield LoadingEventosState();
 
       try {
-        
         ItemModelEventos eventos = await logic.fetchEventos();
         yield MostrarEventosState(eventos);
-
-      }on ListaEventosException{
-        
+      } on ListaEventosException {
         yield ErrorListaEventosState("Sin eventos");
-      
-      }on TokenException{
+      } on TokenException {
         yield ErrorTokenEventosState("Sesión caducada");
       }
-    }else if(event is CreateEventosEvent){
+    } else if (event is CreateEventosEvent) {
       try {
         yield CreateEventosState();
-        Map<String,dynamic> jsonMap = await logic.createEventos(event.data);
-        /*ItemModelEventos model = event.eventos;
-        //String dato = event.data['descripcion'];
-        Map<String,dynamic> lista = {
-          'id_evento':jsonMap['id_evento'],
-          'fecha_inicio':event.data['fecha_inicio'],
-          'fecha_fin':event.data['fecha_fin'],
-          'tipo_evento':'Boda',
-          'involucrados':{"nombre":"Sin nombre", "tipo_involucrado":"Sin involucrado"}
-          };
-        Evento est = new Evento(lista);
-        model.results.add(est);
-        //yield CreateEstatusState(estatus);
-        yield MostrarEventosState(model);*/
-        
+        Map<String, dynamic> jsonMap = await logic.createEventos(event.data);
         add(FechtEventosEvent());
         yield CreateEventosOkState();
-      }on CreateEventoException{
+      } on CreateEventoException {
         yield ErrorCreateEventosState("No se pudo insertar");
+      }
+    } else if (event is FetchEventoPorIdEvent) {
+      yield LoadingEventoPorIdState();
+
+      try {
+        ItemModelEvento evento = await logic.fetchEventoPorId(event.id_evento);
+        yield MostrarEventoPorIdState(evento);
+      } on EventoPorIdException {
+        yield ErrorEventoPorIdState("Sin eventos");
+      } on TokenException {
+        yield ErrorTokenEventosState("Sesión caducada");
       }
     }
   }
--- weddingplanner/lib/src/logic/eventos_logic.dart
+++ weddingplanner/lib/src/logic/eventos_logic.dart
@@ -1,17 +1,23 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
 import 'package:http/http.dart' show Client;
+import 'package:weddingplanner/src/models/item_model_evento.dart';
 import 'package:weddingplanner/src/models/item_model_eventos.dart';
 import 'package:weddingplanner/src/models/item_model_preferences.dart';
 import 'package:weddingplanner/src/resources/config_conection.dart';
 
 abstract class ListaEventosLogic {
   Future<ItemModelEventos> fetchEventos();
-  Future<Map<String,dynamic>> createEventos(Map<String,dynamic> dataEvento);
+  Future<Map<String, dynamic>> createEventos(Map<String, dynamic> dataEvento);
+  Future<ItemModelEvento> fetchEventoPorId(String id_evento);
 }
 
 class ListaEventosException implements Exception {}
+
 class CreateEventoException implements Exception {}
+
+class EventoPorIdException implements Exception {}
 
 class TokenException implements Exception {}
 
@@ -25,7 +31,9 @@ class FetchListaEventosLogic extends ListaEventosLogic {
     int idPlanner = await _sharedPreferences.getIdPlanner();
     String token = await _sharedPreferences.getToken();
     final response = await client.get(
-       Uri.parse(confiC.url+confiC.puerto+'/wedding/EVENTOS/obtenerEventos/$idPlanner'),
+        Uri.parse(confiC.url +
+            confiC.puerto +
+            '/wedding/EVENTOS/obtenerEventos/$idPlanner'),
         headers: {HttpHeaders.authorizationHeader: token});
 
     if (response.statusCode == 200) {
@@ -41,14 +49,49 @@ class FetchListaEventosLogic extends ListaEventosLogic {
   }
 
   @override
-  Future<Map<String, dynamic>> createEventos(Map<String,dynamic> dataEvento) async{
+  Future<ItemModelEvento> fetchEventoPorId(String id_evento) async {
+    int idPlanner = await _sharedPreferences.getIdPlanner();
+    int idEvento = await _sharedPreferences.getIdEvento();
+    String token = await _sharedPreferences.getToken();
+    print('idPlanner -> ' + idPlanner.toString());
+    final response = await client.post(
+        Uri.parse(confiC.url +
+            confiC.puerto +
+            '/wedding/EVENTOS/obtenerEventoPorId/'),
+        headers: {
+          HttpHeaders.authorizationHeader: token
+        },
+        body: {
+          'id_planner': idPlanner.toString(),
+          'id_evento': id_evento.toString()
+        });
+    // var uri = Uri.parse(
+    //     confiC.url + confiC.puerto + '/wedding/EVENTOS/obtenerEventoPorId/');
+    // var header = {HttpHeaders.authorizationHeader: token};
+    // final response = await client.post(uri, headers: header, body: data);
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = json.decode(response.body);
+      await _sharedPreferences.setToken(data['token']);
+      return ItemModelEvento.fromJson(data['data']);
+    } else if (response.statusCode == 401) {
+      await _sharedPreferences.clear();
+      throw TokenException();
+    } else {
+      throw EventoPorIdException();
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> createEventos(
+      Map<String, dynamic> dataEvento) async {
     int idPlanner = await _sharedPreferences.getIdPlanner();
     String token = await _sharedPreferences.getToken();
     int idUsuario = await _sharedPreferences.getIdUsuario();
     dataEvento['id_planner'] = idPlanner.toString();
     dataEvento['id_usuario'] = idUsuario.toString();
     final response = await client.post(
-        Uri.parse(confiC.url+confiC.puerto+'/wedding/EVENTOS/createEventos'),
+        Uri.parse(
+            confiC.url + confiC.puerto + '/wedding/EVENTOS/createEventos'),
         body: dataEvento,
         headers: {HttpHeaders.authorizationHeader: token});
 
