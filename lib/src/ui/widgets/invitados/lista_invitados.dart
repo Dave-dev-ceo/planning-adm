8ad40358ec15da6bf239229335be686a7ce44fbf
Ever <ever@grupotum.com>
2021-11-17 13:53:53 -0600

    Descarga de la plantilla

--- lib/src/ui/widgets/invitados/lista_invitados.dart
+++ lib/src/ui/widgets/invitados/lista_invitados.dart
@@ -1,4 +1,8 @@
 import 'dart:io';
+import 'dart:convert';
+
+import 'package:universal_html/html.dart' as html hide Text;
+import 'package:flutter/services.dart' show ByteData, rootBundle;
 
 import 'package:excel/excel.dart';
 import 'package:file_picker/file_picker.dart';
@@ -87,8 +91,34 @@ class _ListaInvitadosState extends State<ListaInvitados>
         context: context,
         builder: (BuildContext context) => CupertinoAlertDialog(
               title: Text('Importación de excel'),
-              content: Text(
-                  'Procedera a abrir su explorador de archivos para seleccionar un archivo excel,¿Desea continuar?'),
+              content: Column(
+                children: [
+                  Text(
+                      'Procedera a abrir su explorador de archivos para seleccionar un archivo excel,¿Desea continuar?'),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                        primary: Colors.blue,
+                        textStyle: TextStyle(fontSize: 13.0)),
+                    onPressed: () async {
+                      ByteData bytes = await rootBundle.load("Plantilla.xlsx");
+                      ;
+                      final blob = html.Blob([bytes]);
+                      final url = html.Url.createObjectUrlFromBlob(blob);
+
+                      final anchor =
+                          html.document.createElement('a') as html.AnchorElement
+                            ..href = url
+                            ..style.display = 'none'
+                            ..download = 'Plantilla.xlsx';
+                      html.document.body.children.add(anchor);
+                      anchor.click();
+                      html.document.body.children.remove(anchor);
+                      html.Url.revokeObjectUrl(url);
+                    },
+                    child: Text('Descargar plantiila'),
+                  ),
+                ],
+              ),
               actions: <Widget>[
                 CupertinoDialogAction(
                   child: Text(
