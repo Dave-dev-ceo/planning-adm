9f89ab532991f3e6c15cc3112784e47b086d0639
Alejandro Aguilar <alejandro.aguilar@grupotum.com>
2022-01-07 19:04:47 -0600

    Descarga de archivos movil

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -1,5 +1,9 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:external_path/external_path.dart';
+import 'package:flutter/foundation.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:universal_html/html.dart' as html hide Text;
 
 void downloadFile(String data, String titulo, {String extensionFile}) async {
@@ -10,15 +14,27 @@ void downloadFile(String data, String titulo, {String extensionFile}) async {
   }
 
   final bytes = base64Decode(data);
-  final blob = html.Blob([bytes]);
-  final url = html.Url.createObjectUrlFromBlob(blob);
+  if (kIsWeb) {
+    final blob = html.Blob([bytes]);
+    final url = html.Url.createObjectUrlFromBlob(blob);
 
-  final anchor = html.document.createElement('a') as html.AnchorElement
-    ..href = url
-    ..style.display = 'none'
-    ..download = '$titulo-$date.$extensionFile';
-  html.document.body.children.add(anchor);
-  anchor.click();
-  html.document.body.children.remove(anchor);
-  html.Url.revokeObjectUrl(url);
+    final anchor = html.document.createElement('a') as html.AnchorElement
+      ..href = url
+      ..style.display = 'none'
+      ..download = '$titulo-$date.$extensionFile';
+    html.document.body.children.add(anchor);
+    anchor.click();
+    html.document.body.children.remove(anchor);
+    html.Url.revokeObjectUrl(url);
+  } else {
+    PermissionStatus status = await Permission.storage.request();
+    if (status.isGranted) {
+      final path = await ExternalPath.getExternalStoragePublicDirectory(
+          ExternalPath.DIRECTORY_DOWNLOADS);
+      File f = File('$path/pdf.pdf');
+      await f.writeAsBytesSync(bytes);
+    } else {
+      print('NO HAY PERMISO');
+    }
+  }
 }
