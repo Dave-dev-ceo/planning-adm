b95355ee9dfbd4493a0e8b2804fe99c52b017be0
Oswaldo Garcia <ogarcia@PC08.grupotum.com>
2022-06-16 08:44:15 -0500

    quitar print

--- lib/src/blocs/permisos/permisos_bloc.dart
+++ lib/src/blocs/permisos/permisos_bloc.dart
@@ -35,7 +35,7 @@ class PermisosBloc extends Bloc<PermisosEvent, PermisosState> {
 
   @override
   void onTransition(Transition<PermisosEvent, PermisosState> transition) {
-    print(transition);
+    //print(transition);
     super.onTransition(transition);
   }
 }
