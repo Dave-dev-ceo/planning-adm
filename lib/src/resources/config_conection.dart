75b388433f1446ce154d779d8d01d332adab6e0a
Oswaldo Garcia <ogarcia@PC08.grupotum.com>
2022-08-01 11:32:20 -0500

    Nuevos puertos para desarrollo

--- lib/src/resources/config_conection.dart
+++ lib/src/resources/config_conection.dart
@@ -15,7 +15,9 @@ class ConfigConection {
   ConfigConection() {
     if (desarrollo) {
       addPuerto = "3005";
-      addUrl = "http://localhost:";
+      // addUrl = "http://10.0.2.2:"; //Emulador
+      addUrl = "http://localhost:"; //Navegador
+      // addUrl = "http://10.0.1.127:"; //Dispositivo real
     } else {
       addPuerto = "9000";
       addUrl = "https://planning.com.mx:";
