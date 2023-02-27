bb155976eb1d21fd948851f5dc2fabdd69c4330d
emartinez <ever.martinez@grupotum.com>
2023-02-27 13:36:34 -0600

    todo sharedPreferences v1

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,8 @@ class MyHttpOverrides extends HttpOverrides {
   }
 }
 
+// TODO: Comprobar en todas las lecturas de las preferencias de usuario
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HttpOverrides.global = MyHttpOverrides();
