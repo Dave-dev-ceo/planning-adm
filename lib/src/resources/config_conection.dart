4e6fd5bc63687eb2a5e733e31e59a0dc9861b6a1
Yun771 <reve_1997@hotmail.com>
2023-06-05 17:21:37 -0600

    config ip

--- lib/src/resources/config_conection.dart
+++ lib/src/resources/config_conection.dart
@@ -2,6 +2,7 @@ class ConfigConection {
   String? _url;
   String? _puerto;
   bool desarrollo = true;
+
   set addUrl(String data) {
     _url = data;
   }
@@ -11,11 +12,13 @@ class ConfigConection {
   }
 
   String? get url => _url;
+
   String? get puerto => _puerto;
+
   ConfigConection() {
     if (desarrollo) {
       addPuerto = "3005";
-      addUrl = "http://192.168.100.17:"; //Local
+      addUrl = "http://localhost:"; //Local
     } else {
       addPuerto = "9000";
       addUrl = "https://planning.com.mx:"; //Producción
